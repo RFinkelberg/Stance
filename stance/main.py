2b9fbd4e8340ece97e01b96c10d1db9169864329
Aadarsh Padiyath <aspadiyath@gmail.com>
2018-10-25 14:59:18 -0400

    Add template_fit lines to main

--- stance/main.py
+++ stance/main.py
@@ -1,5 +1,6 @@
 import argparse
 import template_fit
+from Stance.stance.motion.squat import Squat
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -8,4 +9,6 @@ if __name__ == "__main__":
     parser.add_argument("-v","--motion_video", help="video containing the user performing a motion")
     args = parser.parse_args()
 
-    template_fit.template_fitting(args.front_view_image, args.profile_view_image)
+    front_view_points, profile_view_points = template_fit.get_points(args.front_view_image, args.profile_view_image)
+
+    motion = Squat(front_view_points, profile_view_points)
