8df71eab722da7fb045fc5aca50dc8323353d325
Roy Finkelberg <roy.finkelberg@gmail.com>
2018-11-19 22:58:48 -0500

    Compute Squat Similarity Scores (#29)
    * Find closest user skeleton to each zone
    
    Adds functionality via `compare_skeletons` to compute cumulative
    similarity scores between skeletons. This is used in
    `find_benchmark_zones_of_user` to identify the closest user skeleton to
    each template skeleton.
    
    * Fix syntax, style errors
    
    * Remove right side of body from scoring

--- stance/motion/Motion.py
+++ stance/motion/Motion.py
@@ -25,11 +25,12 @@ class Motion(ABC):
         """
         raise NotImplementedError
 
-    @staticmethod
+
     @abstractmethod
-    def find_benchmark_zones_of_user(user_skeletons):
+    def find_benchmark_zones_of_user(self, user_skeletons):
         """
-        Compares two skeletons to determine how similar the two skeletons are.
+        Find which user skeletons map closest to the template
+        skeletons
 
         Parameters
         ----------
@@ -43,9 +44,9 @@ class Motion(ABC):
         """
         raise NotImplementedError
 
-    @staticmethod
+
     @abstractmethod
-    def score(user_skeletons):
+    def score(self, user_skeletons):
         """
         Uses user's skeleton to make a score value that defines how close the user
         is to performing the motion perfectly.
