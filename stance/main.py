087606ae1f9298e4195803f1dddc73bbddb385b3
Aadarsh Padiyath <aspadiyath@gmail.com>
2018-11-21 15:39:15 -0500

    Add pickle argument

--- stance/main.py
+++ stance/main.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     # Example calling: python main.py -v etl/squat.mp4
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--motion_video", help="filepath to video containing the user performing a motion")
+    parser.add_argument("-p", "--use_pickle",
+                        help="Number in range(0, 3) which signifies which pickled file to test the code without "
+                             "running the neural network")
     args = parser.parse_args()
 
     print("Starting Motion")
@@ -16,13 +19,22 @@ if __name__ == "__main__":
 
     # Find the user skeletons throughout the video
     print("Fitting Video")
-    user_skeletons = etl.get_user_skeletons(args.motion_video)
-    # with open("squat_user_skeletons.json", "rb") as fp:  # Use with etl/squat.mp4
-    #     user_skeletons = pickle.load(fp)
-    # with open("squatbad_user_skeletons.json", 'rb') as fp:  # Use with etl/squatbad.mp4
-    #     user_skeletons = pickle.load(fp)
-    # with open("squatterrible_user_skeletons.json", 'rb') as fp:  # Use with etl/squatpoop.mp4
-    #     user_skeletons = pickle.load(fp)
+    if not args.use_pickle:
+        user_skeletons = etl.get_user_skeletons(args.motion_video)
+    elif args.use_pickle == str(0):
+        args.motion_video = "example/squat.mp4"
+        with open("squat_user_skeletons.json", "rb") as fp:
+            user_skeletons = pickle.load(fp)
+    elif args.use_pickle == str(1):
+        args.motion_video = "example/squatbad.mp4"
+        with open("squatbad_user_skeletons.json", 'rb') as fp:
+            user_skeletons = pickle.load(fp)
+    elif args.use_pickle == str(2):
+        args.motion_video = "example/squatpoop.mp4"
+        with open("squatterrible_user_skeletons.json", 'rb') as fp:
+            user_skeletons = pickle.load(fp)
+    else:
+        raise Exception("Unknown pickle number, try a number from 0 to 2")
 
     # Find when the user was in a benchmark zone
     print("Finding benchmark zones")
@@ -32,6 +44,7 @@ if __name__ == "__main__":
     # Overlay the template skeletons on the user's video
     print("Overlaying Video")
     overlay.display_overlay(args.motion_video, motion.template_skeletons)
+    # overlay.compare_user_skeletons(args.motion_video, benchmark_zone_indices, user_skeletons, motion)
 
     # Score the user's benchmark zones
     print("Calculating Score")
