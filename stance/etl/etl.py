a47773227fb979e287b5bb4a9586c75c3d010bf7
Aadarsh Padiyath <aspadiyath@gmail.com>
2018-11-27 22:20:01 -0500

    Add Relevant points feature

--- stance/etl/etl.py
+++ stance/etl/etl.py
@@ -8,7 +8,7 @@ from etl.template_fit import get_points_from_image
 from skeleton.skeleton import Skeleton
 
 
-def get_user_skeletons(video_input, verbose=0):
+def get_user_skeletons(video_input, get_points, verbose=0):
     """
     This function takes in a file path to a video of the user performing the motion. It then
     separates the video into frames and performing template_fit's get points from image
@@ -18,6 +18,9 @@ def get_user_skeletons(video_input, verbose=0):
     ----------
     video_input : String
         file path to the video source
+
+    get_points : List[Int]
+        COCO Indices of points to retrieve
     
     verbose : int
         verbosity
@@ -40,7 +43,7 @@ def get_user_skeletons(video_input, verbose=0):
         if image is None:
             break
         start_time = time.time()
-        user_skeletons.append(Skeleton(get_points_from_image(image)))
+        user_skeletons.append(Skeleton(get_points_from_image(image, get_points)))
         logger.debug("Processed frame {} in {} sec".format(i,
                                                            time.time() - start_time))
         i += 1
